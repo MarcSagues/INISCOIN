9b7ff190ec0b9209f5ccaee80a6c6ef88858c2ec
Sagui10 <marc10sagues@gmail.com>
2021-05-19 18:41:41 +0200

    a

--- src/context/axios.js
+++ src/context/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const urlLocalhost = "http://localhost:3001";
+const urlHeroku = "https://inisblockchain.herokuapp.com"
+
 const db = axios.create({
   baseURL: "http://localhost:3001",
 });
